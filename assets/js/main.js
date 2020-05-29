1d1b52eb9a9ed9073b03455d2ea445147d2807ae
Rey David Cuevas <cuevas.vela@gmail.com>
2020-05-29 12:55:49 -0500

    added move function to every row of the board

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
 const Gameboard = () => {
     let board = [null,null,null,null,null,null,null,null,null]
-    function move(index,move){
-        board[index] = move
+    let xSymbol = "<i class=\"fas fa-times fa-7x\">"
+    let oSymbol = "<i class=\"far fa-circle fa-7x\"></i>"
+    let movX = true;
+    function move(index){
+        console.log("move to ");
+        console.log(index);
+        if (movX) {
+            board[index] = "X"
+            movX = !movX;
+        }else {
+            board[index] = "O"
+            movX = !movX;
+        }
+        console.log(board);
+        console.log("---")
     }
     return { move }
 }
@@ -21,7 +34,9 @@ const Game = () => {
         if (validatePlayer()){
             document.getElementById("players-name").classList.add("hide");
             document.getElementById("game-board").classList.remove("hide");
-            gameStart();
+            const player1 = Player(document.getElementById("player1").value, "X");
+            const player2 = Player(document.getElementById("player2").value, "O");
+            gameStart(player1, player2);
         }
     }
 
@@ -37,10 +52,25 @@ const Game = () => {
         }
         return true
     }
+    
 
-    function gameStart() {
-        console.log("started")
+    function gameStart(player1, player2) {
+        board = Gameboard();
+
+        
+        document.getElementById("row-0").addEventListener("click",moveTo(0),true)
+        document.getElementById("row-1").addEventListener("click",moveTo(1),true)
+        document.getElementById("row-2").addEventListener("click",moveTo(2),true)
+        document.getElementById("row-3").addEventListener("click",moveTo(3),true)
+        document.getElementById("row-4").addEventListener("click",moveTo(4),true)
+        document.getElementById("row-5").addEventListener("click",moveTo(5),true)
+        document.getElementById("row-6").addEventListener("click",moveTo(6),true)
+        document.getElementById("row-7").addEventListener("click",moveTo(7),true)
+        document.getElementById("row-8").addEventListener("click",moveTo(8),true)
+
+
     }
+
 
     return {startGame}
 }
