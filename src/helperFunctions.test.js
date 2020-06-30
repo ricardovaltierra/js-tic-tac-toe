8c18231dfb8d09b988bd99bb247044f20153677c
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-06-30 15:21:25 -0500

    added test file for helperFunctions

---
+++ src/helperFunctions.test.js
@@ -0,0 +1,9 @@
+import { validatePlayers,sendAlert } from './helperFunctions.test.js'
+
+test('it returns false with player1 empty string', () => {
+
+});
+
+test('it returns false with player2 empty string', () => {
+
+});
