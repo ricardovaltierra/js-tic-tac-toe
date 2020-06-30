0de90ecf77a682480b65351e814564d8531b49a6
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-06-30 15:04:38 -0500

    added test file for player

---
+++ src/player.test.js
@@ -0,0 +1,9 @@
+import Player from './player';
+
+test('it returns winner symbol X', () => { // Mockup required
+  expect(Player.getName()).toBe("Name");
+});
+
+test('it returns winner symbol X', () => { // Mockup required
+  expect(Player.getSymbol()).toMatch(/X/);
+});
