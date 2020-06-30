dbbd5d7bc60ca55957f5fdd4df40ec6aeada7c5d
Rey David Cuevas <cuevas.vela@gmail.com>
2020-06-30 15:30:09 -0500

    finish test for player

--- test/player.test.js
+++ test/player.test.js
@@ -1,9 +1,23 @@
 import Player from '../src/player';
 
-test('it returns winner symbol X', () => { // Mockup required
-  expect(Player.getName()).toBe("Name");
+test('it returns player name', () => {
+  let player = Player('Name', 'X');
+  expect(player.getName()).toBe("Name");
 });
 
-test('it returns winner symbol X', () => { // Mockup required
-  expect(Player.getSymbol()).toMatch(/X/);
+test('it returns player symbol X', () => {
+  let player = Player('name', 'X');
+  expect(player.getSymbol()).toMatch(/X/);
+});
+
+test('it returns player name if is empty', () => {
+  let player = Player('', 'X');
+  expect(player.getName()).toBe("");
+});
+
+test('should get players symbols', () => {
+  let player1 = Player('one',"O");
+  let player2 = Player('two', 'X');
+  expect(player1.getSymbol()).toBe('O');
+  expect(player2.getSymbol()).toBe('X');
 });
